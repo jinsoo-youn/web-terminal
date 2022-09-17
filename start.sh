b06a7127b22a94d808cdf2c8d07ca40058ea15d6
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2022-09-17 01:37:10 +0000

    Finish dockerfile for webterminal

--- start.sh
+++ start.sh
@@ -6,13 +6,15 @@ env
 echo "export TERM=xterm-256color" >> /root/.bashrc
 echo "source /usr/share/bash-completion/bash_completion" >> /root/.bashrc
 echo 'source <(kubectl completion bash)' >> /root/.bashrc
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
-source ~/.bashrc
+echo 'alias k=kubectl' >> /root/.bashrc
+echo 'complete -o default -F __start_kubectl k' >> /root/.bashrc
 
 echo `kubectl config set-credentials webkubectl-user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)` > /dev/null 2>&1
 echo `kubectl config set-cluster kubernetes --server=$(echo ${KUBERNETES_SERVICE_HOST}):$(echo ${KUBERNETES_SERVICE_PORT}) --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt` > /dev/null 2>&1
 echo `kubectl config set-context kubernetes --cluster=kubernetes --user=webkubectl-user` > /dev/null 2>&1
 echo `kubectl config use-context kubernetes` > /dev/null 2>&1
+
+# helm completion bash 
+# source <(helm completion bash)
 
 ttyd bash
